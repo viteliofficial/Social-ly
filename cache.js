571c1eafd56d902413c72200deb7d021d7fde4b2
netby-official <netbyofficial@gmail.com>
2024-07-18 20:13:58 -0400

    Update

---
+++ cache.js
@@ -0,0 +1,40 @@
+(function() {
+    var version = '1.0.0';  // Change this version number whenever you make updates
+
+    // List of files to update
+    var filesToCacheBust = [
+        'style.css',
+        'script.js',
+        'index.html'
+    ];
+
+    // Function to append version query string to URLs
+    function cacheBust(url) {
+        return url + '?v=' + version;
+    }
+
+    // Update link tags for CSS
+    var linkTags = document.querySelectorAll('link[rel="stylesheet"]');
+    linkTags.forEach(function(link) {
+        link.href = cacheBust(link.href);
+    });
+
+    // Update script tags for JavaScript
+    var scriptTags = document.querySelectorAll('script[src]');
+    scriptTags.forEach(function(script) {
+        script.src = cacheBust(script.src);
+    });
+
+    // Update other specific files
+    filesToCacheBust.forEach(function(file) {
+        var elements = document.querySelectorAll('[src="' + file + '"], [href="' + file + '"]');
+        elements.forEach(function(element) {
+            if (element.src) {
+                element.src = cacheBust(element.src);
+            }
+            if (element.href) {
+                element.href = cacheBust(element.href);
+            }
+        });
+    });
+})();
